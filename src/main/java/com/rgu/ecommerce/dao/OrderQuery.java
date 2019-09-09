fd3f87f42cde7fad324101dbc357c4cff5c9cc91
khakholiank <48200142+khakholiank@users.noreply.github.com>
2019-09-09 16:11:21 +0530

    commit 1

---
+++ src/main/java/com/rgu/ecommerce/dao/OrderQuery.java
@@ -0,0 +1,40 @@
+
+package com.rgu.ecommerce.dao;
+
+/**
+ *
+ * @author Nikit Khakholia
+ */
+
+//    private int id;
+//    private int sellerId;
+//    private int custId;
+//    private LocalDateTime timeOfOrder;
+//    private Address deliveryAddress;
+//    private Address billingAddress;
+//    private Product product;
+//    private int qty;
+//    private String trackingId;
+//    private LocalDateTime estDeliveryTime;
+//    private String promoCode;
+//    private double grossAmount;
+//    private double discApplied;
+//    private double deliveryFee;
+//    private double netAmount;
+//    private OrderStatus orderStatus;
+
+
+//    private int addressId;
+//    private int userId;
+//    private String line1;
+//    private String line2;
+//    private Locality locality;
+//    private City city;
+//    private State state;
+//    private Country country;
+//    private int phone;
+public class OrderQuery {
+    
+    private static final String ADD = "INSERT INTO order(id,seller_id,cust_id,time_of_order,d_address_id,) VALUES()";
+    
+}
