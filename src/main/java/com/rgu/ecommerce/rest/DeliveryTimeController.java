c5286f69e2c260f2355baf12f96c947d2453d621
khakholiank <48200142+khakholiank@users.noreply.github.com>
2019-09-14 22:27:07 +0530

    commit 1

---
+++ src/main/java/com/rgu/ecommerce/rest/DeliveryTimeController.java
@@ -0,0 +1,17 @@
+
+package com.rgu.ecommerce.rest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Nikit Khakholia
+ */
+@RestController
+@RequestMapping("deliverytime/")
+public class DeliveryTimeController {
+    @PostMapping("add")
+    public 
+}
