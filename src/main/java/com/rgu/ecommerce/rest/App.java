de8cc6ef851df2b83ba774dfc170d0d076600d3a
khakholiank <48200142+khakholiank@users.noreply.github.com>
2019-09-15 18:44:27 +0530

    commit 1

---
+++ src/main/java/com/rgu/ecommerce/rest/App.java
@@ -0,0 +1,34 @@
+
+package com.rgu.ecommerce.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author Shriram Prajapat
+ */
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { JacksonAutoConfiguration.class })
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");//.allowedOrigins("*");
+            }
+        };
+    }
+}
