334d7eb198bd7cf34f989a4c45261023f6e6f1cf
khakholiank <48200142+khakholiank@users.noreply.github.com>
2019-09-15 18:03:13 +0530

    commit 1

--- src/main/java/com/rgu/ecommerce/dao/OrderQuery.java
+++ src/main/java/com/rgu/ecommerce/dao/OrderQuery.java
@@ -1,8 +1,6 @@
-
 package com.rgu.ecommerce.dao;
 
 import com.rgu.ecommerce.commons.Address;
-import com.rgu.ecommerce.commons.Locality;
 import com.rgu.ecommerce.config.OrderStatus;
 import com.rgu.ecommerce.model.Order;
 import com.rgu.ecommerce.model.Product;
@@ -17,34 +15,31 @@ import java.util.List;
  *
  * @author Nikit Khakholia
  */
-
-
 public class OrderQuery {
-    
+
     private static final String ADD = "INSERT INTO order(id,seller_id,cust_id,time_of_order,d_address_id,d_line_1,d_line_2,d_locality_id,d_city_id,"
             + "d_state_id,d_country_id,d_phone,b_address_id,b_line_1,b_line_2,b_locality_id,b_city_id,b_state,b_country,b_phone,item_id,item_name,item_desc,"
             + "item_img_media_id,qty,tracking_id,est_delivery_time,promocode,gross_amt,disc_applied,delivery_fee,net_amount,order_status_code) "
             + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-    
+
     private static final String UPDATE = "UPDATE order SET seller_id=?,cust_id=?,time_of_order=?,d_address_id=?,d_line_1=?,d_line_2=?,d_locality_id=?,d_city_id=?,"
             + "d_state=?,d_country=?,d_phone=?,b_address_id=?,b_line_1=?,b_line_2=?,b_locality_id=?,b_city_id=?,b_state=?,b_country=?,b_phone=?,item_id=?,item_name=?,item_desc=?,"
             + "item_img_media_id=?,qty=?,tracking_id=?,est_delivery_time=?,promocode=?,gross_amt=?,disc_applied=?,delivery_fee=?,net_amount=?,order_status_code=?WHERE id=?";
-    
-    private static final String DELETE = "DELETE FROM order WHERE id=?";
-    
-    private static final String SELECT_BY_ORDER_ID = "SELECT * FROM order WHERE id = ?";
-    
-    private static final String SELECT_BY_CUSTOMER_ID = "SELECT * FROM order WHERE cust_id = ?";
-    
-    private static final String SELECT_BY_SELLER_ID = "SELECT * FROM order WHERE seller_id = ?";
-    
 
-    private static final String SELECT_ALL = "SELECT * FROM order";   
-    
-    public static boolean add(Order o){
+    private static final String DELETE = "DELETE FROM order WHERE id=?";
+
+    private static final String SELECT_BY_ORDER_ID = "SELECT * FROM order WHERE id = ?";
+
+    private static final String SELECT_BY_CUSTOMER_ID = "SELECT * FROM order WHERE cust_id = ?";
+
+    private static final String SELECT_BY_SELLER_ID = "SELECT * FROM order WHERE seller_id = ?";
+
+    private static final String SELECT_ALL = "SELECT * FROM order";
+
+    public static boolean add(Order o) {
         boolean success = false;
-        try(Connection con = Conn.getConnection();
-                PreparedStatement ps = con.prepareStatement(ADD)){
+        try (Connection con = Conn.getConnection();
+                PreparedStatement ps = con.prepareStatement(ADD)) {
             ps.setInt(1, o.getId());
             ps.setInt(2, o.getSellerId());
             ps.setInt(3, o.getCustId());
@@ -78,20 +73,20 @@ public class OrderQuery {
             ps.setDouble(31, o.getDeliveryFee());
             ps.setDouble(32, o.getNetAmount());
             ps.setInt(33, o.getOrderStatus().getCode());
-            
-            success= ps.executeUpdate()==1;
-            
-        }catch(SQLException ex){
+
+            success = ps.executeUpdate() == 1;
+
+        } catch (SQLException ex) {
             System.err.println(ex.toString());
             return success;
         }
         return success;
     }
 
-    public static boolean update(Order o){
+    public static boolean update(Order o) {
         boolean success = false;
-        try(Connection con = Conn.getConnection();
-                PreparedStatement ps = con.prepareStatement(UPDATE)){
+        try (Connection con = Conn.getConnection();
+                PreparedStatement ps = con.prepareStatement(UPDATE)) {
             ps.setInt(1, o.getSellerId());
             ps.setInt(2, o.getCustId());
             ps.setTimestamp(3, java.sql.Timestamp(o.getTimeOfOrder()));
@@ -124,32 +119,32 @@ public class OrderQuery {
             ps.setDouble(30, o.getDeliveryFee());
             ps.setDouble(31, o.getNetAmount());
             ps.setInt(32, o.getOrderStatus().getCode());
-            
+
             ps.setInt(33, o.getId());
 
-            success= ps.executeUpdate()==1;
-            
-        }catch(SQLException ex){
+            success = ps.executeUpdate() == 1;
+
+        } catch (SQLException ex) {
             System.err.println(ex.toString());
             return success;
         }
         return success;
     }
-    
-    public static boolean delete(int id){
+
+    public static boolean delete(int id) {
         boolean success = false;
-        try(Connection con = Conn.getConnection();
-                PreparedStatement ps = con.prepareStatement(DELETE)){
+        try (Connection con = Conn.getConnection();
+                PreparedStatement ps = con.prepareStatement(DELETE)) {
             ps.setInt(1, id);
-            success = ps.executeUpdate()==1;
-        }catch(SQLException ex){
+            success = ps.executeUpdate() == 1;
+        } catch (SQLException ex) {
             System.err.println(ex.toString());
             return success;
         }
         return success;
     }
-    
-    private static Order mapObject(ResultSet rs)throws SQLException{
+
+    private static Order mapObject(ResultSet rs) throws SQLException {
         Order o = new Order();
         o.setId(rs.getInt(1));
         o.setSellerId(rs.getInt(2));
@@ -165,7 +160,7 @@ public class OrderQuery {
         dA.setCountry(rs.getInt(11));
         dA.setPhone(rs.getInt(12));
         o.setDeliveryAddress(dA);
-        
+
         Address bA = new Address();
         bA.setAddressId(rs.getInt(13));
         bA.setLine1(rs.getString(14));
@@ -185,7 +180,7 @@ public class OrderQuery {
         p.setDescription(rs.getString(23));
         p.setImgMediaId(rs.getInt(24));
         o.setProduct(p);
-        
+
         o.setQty(rs.getInt(25));
         o.setTrackingId(rs.getString(26));
         o.setEstDeliveryTime(rs.getTimestamp(27).toLocalDateTime());
@@ -195,74 +190,75 @@ public class OrderQuery {
         o.setDeliveryFee(rs.getDouble(31));
         o.setNetAmount(rs.getDouble(32));
         o.setOrderStatus(OrderStatus.valueOf(rs.getInt(33)));
-        
+
         return o;
     }
-    
-    public static List<Order> getOrderById(int id){
+
+    public static List<Order> getOrderById(int id) {
         List<Order> list = new ArrayList<>();
-        try(Connection con = Conn.getConnection();
-                PreparedStatement ps = con.prepareStatement(SELECT_BY_ORDER_ID)){
+        try (Connection con = Conn.getConnection();
+                PreparedStatement ps = con.prepareStatement(SELECT_BY_ORDER_ID)) {
             ps.setInt(1, id);
-            try(ResultSet rs = ps.executeQuery()){
-                while(rs.next()){
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
                     list.add(mapObject(rs));
                 }
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println(ex.toString());
             return null;
         }
         return list;
     }
-    
-    public static List<Order> getOrderByCustId(int id){
+
+    public static List<Order> getOrderByCustId(int id) {
         List<Order> list = new ArrayList<>();
-        try(Connection con = Conn.getConnection();
-                PreparedStatement ps = con.prepareStatement(SELECT_BY_CUSTOMER_ID)){
+        try (Connection con = Conn.getConnection();
+                PreparedStatement ps = con.prepareStatement(SELECT_BY_CUSTOMER_ID)) {
             ps.setInt(1, id);
-            try(ResultSet rs = ps.executeQuery()){
-                while(rs.next()){
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
                     list.add(mapObject(rs));
                 }
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println(ex.toString());
             return null;
         }
         return list;
     }
-    
-    public static List<Order> getOrderBySellerId(int id){
+
+    public static List<Order> getOrderBySellerId(int id) {
         List<Order> list = new ArrayList<>();
-        try(Connection con = Conn.getConnection();
-                PreparedStatement ps = con.prepareStatement(SELECT_ALL)){
+        try (Connection con = Conn.getConnection();
+                PreparedStatement ps = con.prepareStatement(SELECT_BY_SELLER_ID)) {
             ps.setInt(1, id);
-            try(ResultSet rs = ps.executeQuery()){
-                while(rs.next()){
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
                     list.add(mapObject(rs));
                 }
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println(ex.toString());
             return null;
         }
         return list;
     }
-    public static List<Order> getAllOrders(){
+
+    public static List<Order> getAllOrders() {
         List<Order> list = new ArrayList<>();
-        try(Connection con = Conn.getConnection();
-                PreparedStatement ps = con.prepareStatement(SELECT_BY_ORDER_ID)){
-            try(ResultSet rs = ps.executeQuery()){
-                while(rs.next()){
+        try (Connection con = Conn.getConnection();
+                PreparedStatement ps = con.prepareStatement(SELECT_ALL)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
                     list.add(mapObject(rs));
                 }
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println(ex.toString());
             return null;
         }
         return list;
     }
-    
+
 }
